b4387465041dd38c603e11a53bf7d2de0999d306
gonzalky7 <gonzalky@oregonstate.edu>
2018-04-27 16:27:18 -0700

    Beginning to implement FCFS

--- Scheduling/Scheduling/main.cpp
+++ Scheduling/Scheduling/main.cpp
@@ -7,18 +7,48 @@
 //
 
 #include <iostream>
+#include <fstream>
+#include <map>
+#include <iterator>
 
 using namespace std;
+multimap<int, int> fcfs_multimap;
+void fcfs(int pid, int arrival, int burst) {
+    
+  
+    fcfs_multimap.insert(pair<int, int> (pid, burst));
+    
+
+}
 
 int main(int argc, const char * argv[]) {
     //UNIX> ./hw4 sim_time algorithm [time_slice]
+
     
-    cout << "You have entered " << argc
-    << " arguments:" << "\n";
+    string sim_time = argv[1];
+    string algorithm = argv[2];
     
-    for (int i = 0; i < argc; ++i)
-        cout << argv[i] << "\n";
+    string line;
     
+//    int tmp;
+    while (!cin.eof()) {
+        int pid ;
+        int arrival;
+        int burst ;
+        
+        cin >> pid >> arrival >> burst;
+        if (algorithm == "FCFS"){
+            fcfs(pid, arrival, burst);
+        }
+    }
+    multimap <int, int> :: iterator itr;
+    cout << "\tPID\tBURST TIME\n";
+    for (itr = fcfs_multimap.begin(); itr != fcfs_multimap.end(); ++itr)
+    {
+        cout  <<  '\t' << itr->first
+        <<  '\t' << itr->second << '\n';
+    }
+    cout << endl;
     
     return 0;
 }
