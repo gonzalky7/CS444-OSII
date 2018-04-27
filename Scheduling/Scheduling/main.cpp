bff190858b40ce46dd12f83e7d16b12ec51b8308
gonzalky7 <gonzalky@oregonstate.edu>
2018-04-26 19:23:36 -0700

    Adding homework 4

---
+++ Scheduling/Scheduling/main.cpp
@@ -0,0 +1,23 @@
+//
+//  main.cpp
+//  Scheduling
+//
+//  Created by Kyleen Gonzalez on 4/24/18.
+//  Copyright © 2018 Kyleen Gonzalez. All rights reserved.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    
+    cout << "You have entered " << argc
+    << " arguments:" << "\n";
+    
+    for (int i = 0; i < argc; ++i)
+        cout << argv[i] << "\n";
+    
+    
+    return 0;
+}
